200d084cb011d4b0ed4640347ad05361f66aa604
Marta Moreno <mccm20@gmail.com>
2018-03-12 14:35:36 +0000

    Added process command

--- vcaller.py
+++ vcaller.py
@@ -1,7 +1,7 @@
-import click
-from subprocess import run
 import os
+from subprocess import run
 
+import click
 
 ##### IMPORT CONFIG #####
 config = {}
@@ -65,7 +65,6 @@ def align_bwa(name, nthreads, check_index, clean_up, reference, read1, read2):
     It is only mandatory to include the reference genome file and a sample read as arguments.
     If dealing with paired-end reads, a second sequence file containing the second mate-pair read may be included."""
 
-
     # (FLAG) check_index: check if reference genome is already indexed
     if check_index:
         suffix_list = ['.amb', '.ann', '.bwt', '.pac', '.sa']
@@ -78,13 +77,12 @@ def align_bwa(name, nthreads, check_index, clean_up, reference, read1, read2):
         click.echo('Indexing reference genome %s...' % reference)
         run(['bwa', 'index', reference])
 
-
     # Align input sequences to the reference genome
-    click.echo('Aligning read(s) against the reference genome...') # find way to make message fancier with custom names
+    click.echo('Aligning read(s) against the reference genome...')  # find way to make message fancier with custom names
     align_args = ['bwa', 'mem', '-M', '-t', nthreads, reference, read1]
     if read2 is not None:
         align_args += read2
-    output_name=name+'.sam'
+    output_name = name + '.sam'
     with open(output_name, "w") as align_out:
         print(align_args)
         run(align_args, stdout=align_out)
@@ -94,14 +92,15 @@ def align_bwa(name, nthreads, check_index, clean_up, reference, read1, read2):
     sort_args = ['samtools', 'sort', '-O', 'bam', '-o', name + '.bam', '-T', '/tmp/lane_temp', output_name]
     run(sort_args)
 
-
     # clean up intermediary files
     if clean_up:
         click.echo('Cleaning up %s...' % output_name)
         run(['rm', name + output_name])
 
+
 @align.command('bowtie2')
-@click.option('--name', '-n', default='bowtie2_out', help='Name of the output file (extension will be added automatically)')
+@click.option('--name', '-n', default='bowtie2_out',
+              help='Name of the output file (extension will be added automatically)')
 @click.option('--check_index', '-i', is_flag=True, help='Check if reference is already indexed, and if yes prompt '
                                                         'the user to skip that step.')
 @click.option('--clean-up', '-c', default=False, help='Clean up intermediary files to save disk space.')
@@ -113,8 +112,7 @@ def align_bwa(name, check_index, clean_up, reference, read1, read2):
     It is only mandatory to include the reference genome file and a sample read as arguments.
     If dealing with paired-end reads, a second sequence file containing the second mate-pair read may be included."""
 
-
-    ref_basename = reference.split('.')[0] # bowtie2 uses the reference's basename (no suffix) a lot
+    ref_basename = reference.split('.')[0]  # bowtie2 uses the reference's basename (no suffix) a lot
     # (FLAG) check_index: check if reference genome is already indexed
     if check_index:
         suffix_list = ['.1.bt2', '.2.bt2', '.3.bt2', '.4.bt2', '.rev.1.bt2', '.rev.2.bt2']
@@ -123,46 +121,35 @@ def align_bwa(name, check_index, clean_up, reference, read1, read2):
         else:
             click.echo('Need to generate index files!\n Indexing reference genome %s...' % reference)
             # Index the reference genome
-            index_args = [config['bowtie2_path']+'/bowtie2-build', reference, ref_basename] # last arg is the
-                                                                                            # output name
+            index_args = [config['bowtie2_path'] + '/bowtie2-build', reference, ref_basename]  # last arg is the
+            # output name
             run(index_args)
     else:
         click.echo('Indexing reference genome %s...' % reference)
-        index_args = [config['bowtie2_path']+'/bowtie2-build', reference, ref_basename]
+        index_args = [config['bowtie2_path'] + '/bowtie2-build', reference, ref_basename]
         run(index_args)
-
 
     # ADD MULTI-THREAD OPTION -p !!
     # Align the input reads against the reference genome
-    output_name = name+'.sam'
-    if read2 is None: # if read is single-ended
-        align_args = [config['bowtie2_path']+'/bowtie2', '-x', ref_basename, read1, '-S', output_name]
+    sam_output = name + '.sam'
+    if read2 is None:  # if read is single-ended
+        align_args = [config['bowtie2_path'] + '/bowtie2', '-x', ref_basename, read1, '-S', sam_output]
         click.echo('Aligning read %s against the reference genome...' % read1)
-    else: # if paired_end
-        align_args = [config['bowtie2_path']+'/bowtie2', '-x', ref_basename,
-                      '-1', read1, '-2', read2, '-S', output_name]
+    else:  # if paired_end
+        align_args = [config['bowtie2_path'] + '/bowtie2', '-x', ref_basename,
+                      '-1', read1, '-2', read2, '-S', sam_output]
         click.echo('Aligning reads %s %s against the reference genome...' % (read1, read2))
     run(align_args)
 
-
     # Sort and convert to BAM
     click.echo('Sorting and converting to BAM...')
-    sort_args = ['samtools', 'sort', '-O', 'bam', '-o', name + '.bam', '-T', '/tmp/lane_temp', name + output_name]
+    sort_args = ['samtools', 'sort', '-O', 'bam', '-o', name + '.bam', '-T', '/tmp/lane_temp', sam_output]
     run(sort_args)
-
 
     # clean up intermediary files
     if clean_up:
-        click.echo('Cleaning up %s...' % output_name)
-        run(['rm', output_name])
-
-
-##### POST-PROCESSING #####
-@cli.command(short_help='Prepare reads for variant calling.')
-@click.argument('asjfaj')
-def processing(asjfaj):
-    """Performs a group of steps for the post-processing of aligned reads in preparation for variant calling."""
-    pass
+        click.echo('Cleaning up %s...' % sam_output)
+        run(['rm', sam_output])
 
 
 ##### VARIANT CALLING #####
@@ -172,7 +159,8 @@ def call():
 
 
 @call.command('gatk')
-@click.option('--name', '-n', default='gatk_out', help='Name of the output file (extension will be added automatically)')
+@click.option('--name', '-n', default='gatk_out',
+              help='Name of the output file (extension will be added automatically)')
 @click.option('--platform', '-p', default='ion proton', help='Platform used in sample sequencing.')
 @click.option('--library', '-l', default='library1', help='DNA preparation library identifier.')
 @click.option('--sample', '-s', default='NA12878', help='The name of the sample sequenced in a read group.')
@@ -202,61 +190,59 @@ def call_gatk(name, platform, library, sample, known_snps, clean_up, reference, 
     """
 
     # samtools faidx on REFERENCE
-    if check_existence([reference+'.fai']):
+    if check_existence([reference + '.fai']):
         click.echo('Reference faidx index file already exists!\nSkipping faidx indexing.')
     else:
         click.echo('Indexing reference file %s...' % reference)
         faidx_args = ['samtools', 'faidx', reference]
         run(faidx_args)
 
-
     # generate .dict dictionary file for REFERENCE
-    dict_file = reference.split('.')[0]+'.dict'
+    dict_file = reference.split('.')[0] + '.dict'
     if check_existence([dict_file]):
-        click.echo('Dictionary file %s already exists.\nSkipping reference genome dictionary file generation.' % dict_file)
+        click.echo(
+            'Dictionary file %s already exists.\nSkipping reference genome dictionary file generation.' % dict_file)
     else:
         click.echo('Generating reference genome dictionary %s...' % dict_file)
         dict_vars = ['java', '-jar', config['picard_path'], 'CreateSequenceDictionary', 'R=%s' % reference,
                      'O=%s' % dict_file]
         run(dict_vars)
 
-
-    # read groups on each SAMPLE$BT
+    # read groups on each SAMPLE
     # only works for 1 library atm
     sample_list = [sample1] + [s for s in sample2]
-    sample_rg_list = [sample.split('.')[0]+'_rg.'+sample.split('.')[1] for sample in sample_list]
-    if check_existence(sample_rg_list):
+    sample_rg_list = [sample.split('.')[0] + '_rg.' + sample.split('.')[1] for sample in sample_list]
+    if check_existence([sample_rg_list]):
         click.echo('Read group information has already been added for %s.' % ', '.join(sample_list))
     else:
-        for spl in sample_list:
-            click.echo('Adding Read Group information for %s...' % spl)
-            read_groups = {'ID': spl.split('.')[0], 'PL': platform, 'LB': library, 'PU': 'foo', 'SM': sample}
-            rg_args = ['java', '-jar', config['picard_path'], 'AddOrReplaceReadGroups', 'I='+spl,
-                       'O='+spl.split('.')[0]+'_rg.bam', 'RGID='+read_groups['ID'], 'RGLB='+read_groups['LB'],
-                       'RGPL='+read_groups['PL'], 'RGPU='+read_groups['PU'], 'RGSM='+read_groups['SM']]
+        for smpl in sample_list:
+            click.echo('Adding Read Group information for %s...' % smpl)
+            read_groups = {'ID': smpl.split('.')[0], 'PL': platform, 'LB': library, 'PU': 'foo', 'SM': sample}
+            rg_args = ['java', '-jar', config['picard_path'], 'AddOrReplaceReadGroups', 'I=' + smpl,
+                       'O=' + smpl.split('.')[0] + '_rg.bam', 'RGID=' + read_groups['ID'], 'RGLB=' + read_groups['LB'],
+                       'RGPL=' + read_groups['PL'], 'RGPU=' + read_groups['PU'], 'RGSM=' + read_groups['SM']]
             run(rg_args)
 
-
     # samtools index on each SAMPLE
-    if check_existence([sample+'.bai' for sample in sample_rg_list]):
+    if check_existence([sample + '.bai' for sample in sample_rg_list]):
         click.echo('Sample index .bai files already exist!\nSkipping sample indexing.')
     else:
         click.echo('Need to generate sample index .bai files!\nIndexing sample files %s...' % ', '.join(sample_rg_list))
         index_args = ['samtools', 'index'] + sample_rg_list
         run(index_args)
 
-
     # run GATK-HC
     click.echo('Calling variants on samples %s with GATK-HC...' % ', '.join(sample_rg_list))
     if known_snps is None:
         # each sample needs to be preceed by an -I so this is not working for more than one sample?
-        gatk_args = ['java', '-jar', config['gatk_path'], '-R', reference, '-T', 'HaplotypeCaller', '-I'] + sample_rg_list + \
-                ['-stand_call_conf', '20', '-o', name+'.vcf'] # confidence of call is not flexible atm, always 20
+        gatk_args = ['java', '-jar', config['gatk_path'], '-R', reference, '-T', 'HaplotypeCaller',
+                     '-I'] + sample_rg_list + \
+                    ['-stand_call_conf', '20', '-o', name + '.vcf']  # confidence of call is not flexible atm, always 20
     else:
-        gatk_args = ['java', '-jar', config['gatk_path'], '-R', reference, '-T', 'HaplotypeCaller', '-I'] + sample_rg_list + \
-                    ['--dbsnp', known_snps, '-stand_call_conf', '20', '-o', name+'.vcf']
+        gatk_args = ['java', '-jar', config['gatk_path'], '-R', reference, '-T', 'HaplotypeCaller',
+                     '-I'] + sample_rg_list + \
+                    ['--dbsnp', known_snps, '-stand_call_conf', '20', '-o', name + '.vcf']
     run(gatk_args)
-
 
     # clean up intermediary files
     if clean_up:
@@ -266,7 +252,8 @@ def call_gatk(name, platform, library, sample, known_snps, clean_up, reference, 
 
 
 @call.command('bcftools')
-@click.option('--name', '-n', default='bcftools_out', help='Name of the output file (extension will be added automatically)')
+@click.option('--name', '-n', default='bcftools_out',
+              help='Name of the output file (extension will be added automatically)')
 @click.option('--clean-up', '-c', default=False, help='Clean up intermediary files to save disk space.')
 @click.argument('reference', type=click.Path(exists=True))
 @click.argument('sample1', type=click.Path(exists=True))
@@ -283,13 +270,12 @@ def call_bcftools(name, clean_up, reference, sample1, sample2):
     # PROBLEM: CURRENTLY FINDS MORE VARIANTS THAN IT SHOULD?
 
     sample_list = [sample1] + [s for s in sample2]
-    bcf_output = name+'.bcf'
-    vcf_output = name+'.vcf'
+    bcf_output = name + '.bcf'
+    vcf_output = name + '.vcf'
     # bcftools mpileup
     click.echo('Calculating genotype likelihoods for %s...' % ', '.join(sample_list))
     mpileup_args = ['bcftools', 'mpileup', '-Ob', '-o', bcf_output, '-f', reference] + sample_list
     run(mpileup_args)
-
 
     # variant calling
     click.echo('Calling variants on %s with BCFtools...' % ', '.join(sample_list))
@@ -300,3 +286,87 @@ def call_bcftools(name, clean_up, reference, sample1, sample2):
     if clean_up:
         click.echo('Cleaning up %s...' % bcf_output)
         run(['rm', bcf_output])
+
+
+##### POST-PROCESSING #####
+@cli.command('process', short_help='Prepare reads for variant calling.')
+@click.option('--platform', '-p', default='IONTORRENT', help='Platform used in sample sequencing.')
+@click.option('--library', '-l', default='library1', help='DNA preparation library identifier.')
+@click.option('--sample', '-s', default='NA12878', help='The name of the sample sequenced in a read group.')
+# Try to make these 2 options, to see if it's possible to give more than 1 set
+@click.argument('known-indels', required=True)
+@click.argument('known-snps', required=True)
+@click.argument('reference', required=True)
+@click.argument('samples', required=True, type=click.Path(exists=True), nargs=-1)
+def process(platform, library, sample, known_indels, known_snps, reference, samples):
+    """Performs a group of steps for the post-processing in preparation for variant calling
+    on one or more SAM/BAM sample files."""
+    sample_list = list(samples)
+
+    # Create a directory structure to store post-processed files, only if it does not exist yet
+    # run(['mkdir', '-p', 'post-processing-tmp'])
+
+    ##### STORE MORE STUFF IN THE TMP FOLDER
+    # Process each sample at a time
+    for smpl in sample_list:
+        smpl_name, smpl_extension = smpl.split('.')
+        smpl_extension = '.' + smpl_extension
+
+        # read groups
+        # only works for 1 library atm
+        rg_output = smpl_name + '.RG' + smpl_extension
+        if not check_existence([rg_output]):
+            click.echo('Adding Read Group information to %s...' % smpl)
+            read_groups = {'ID': smpl_name, 'PL': platform, 'LB': library, 'PU': 'foo', 'SM': sample}
+            rg_args = ['java', '-jar', config['picard_path'], 'AddOrReplaceReadGroups', 'I=' + smpl,
+                       'O=' + rg_output, 'RGID=' + read_groups['ID'], 'RGLB=' + read_groups['LB'],
+                       'RGPL=' + read_groups['PL'].upper(), 'RGPU=' + read_groups['PU'], 'RGSM=' + read_groups['SM']]
+            run(rg_args)
+        # NOTE: "ERROR MESSAGE: The platform (ion proton) associated with read group GATKSAMReadGroupRecord
+        # @RG:bowtie2_out is not a recognized platform. Allowable options are ILLUMINA,SLX,SOLEXA,SOLID,454,LS454,
+        # COMPLETE,PACBIO,IONTORRENT,CAPILLARY,HELICOS,UNKNOWN
+
+        # Mark and remove duplicates (make it an option to not remove, only marking?)
+        dup_output = '.'.join(rg_output.split('.')[:-1]) + '.DUP' + smpl_extension
+        if not check_existence([dup_output]):
+            click.echo('Marking and removing duplicates for %s...' % smpl_name)
+            # intermediary file
+            dup_args = ['java', '-jar', config['picard_path'], 'MarkDuplicates', 'I=' + rg_output,
+                        'O=' + dup_output, 'REMOVE_DUPLICATES=true',
+                        'M=' + smpl_name + '.metrics']
+            run(dup_args)
+
+        # Realign around indels
+        # Preparation: samtools index
+        if not check_existence([dup_output + '.bai']):
+            click.echo('Indexing %s...' % dup_output)
+            run(['samtools', 'index', dup_output])
+        # Known indels HAVE to be indexed... how to ensure?
+        intervals_output = smpl_name + '.intervals'
+        if not check_existence([intervals_output]):
+            click.echo('Creating indel realignment intervals for %s...' % smpl_name)
+            intervals_args = ['java', '-jar', config['gatk_path'], '-T', 'RealignerTargetCreator', '-R', reference,
+                              '-I', dup_output, '-o', intervals_output, '--known',
+                              known_indels]  # only one set of known atm
+            run(intervals_args)
+        realign_output = '.'.join(dup_output.split('.')[:-1]) + '.RLGN' + smpl_extension
+        if not check_existence([realign_output]):
+            click.echo('Applying indel realignment based on the intervals for %s...' % smpl_name)
+            realign_args = ['java', '-jar', config['gatk_path'], '-T', 'IndelRealigner', '-R', reference,
+                            '-I', dup_output, '-targetIntervals', intervals_output, '-known', known_indels,
+                            '-o', realign_output]
+            run(realign_args)
+
+        # BQSR --- NOT WORKING!!
+        table_output = smpl_name + '.table'  # should be the ACTUAL name for the file...
+        if not check_existence([table_output]):
+            click.echo('Creating base score recalibration table for %s...' % smpl_name)
+            table_args = ['java', '-jar', config['gatk_path'], '-T', 'BaseRecalibrator', '-R', reference,
+                          '-knownSites', known_snps, '-I', realign_output, '-o', table_output]
+            run(table_args)
+        bqsr_output = '.'.join(realign_output.split('.')[:-1]) + '.BQSR' + smpl_extension
+        if not check_existence([bqsr_output]):
+            click.echo('Running base score recalibration on %s...' % smpl_name)
+            bqsr_args = ['java', '-jar', config['gatk_path'], '-T', 'PrintReads', '-R', reference,
+                         '-I', realign_output, '--BQSR', table_output, '-o', bqsr_output]
+            run(bqsr_args)
